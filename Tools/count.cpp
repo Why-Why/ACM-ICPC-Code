7d691056d3f7582ebe5f85530f2ff1dadb3a8d16
Why-Why <daxiaonewwhy@163.com>
2015-12-21 19:58:47 +0800

    Tools

---
+++ Tools/count.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <cstdio>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+bool judge(char *name)
+{
+	int rem=-1;
+	for(int i=0;name[i];++i)
+		if(name[i]=='.')
+			rem=i;
+
+	if(rem==-1)
+		return 0;
+
+	if(strcmp("c",name+rem+1) && strcmp("cpp",name+rem+1) && strcmp("h",name+rem+1))
+		return 0;
+
+	return 1;
+}
+
+int count_file(char *name)
+{
+	if(judge(name)==0) return 0;
+
+	cout<<name<<" : ";
+
+	freopen(name,"r",stdin);
+	int t;
+	int ret=0;
+
+	while((t=getchar())!=-1)
+		if(t=='\n')
+			++ret;
+
+	cout<<ret<<endl;
+	fclose(stdin);
+	return ret;
+}
+
+int count_dir(char *path)
+{
+	DIR *d;
+	struct dirent * file;
+	struct stat sb;
+	char ts[256];
+	int ret=0;
+
+	if(!(d=opendir(path)))
+	{
+		cerr<<"Error Opendir "<<path<<endl;
+		return 0;
+	}
+
+	while((file=readdir(d))!=0)
+	{
+		if(file->d_name[0]=='.')
+			continue;
+		strcpy(ts,path);
+		strcat(ts,"/");
+		strcat(ts,file->d_name);
+
+		if(stat(ts,&sb)<0) continue;
+
+		if(S_ISDIR(sb.st_mode))
+			ret+=count_dir(ts);
+		else
+			ret+=count_file(ts);
+	}
+
+	return ret;
+}
+
+int main(int argc,char **argv)
+{
+	if(argc<2)
+	{
+		cout<<"Please give a paht.\n";
+		return 1;
+	}
+
+	cout<<endl;
+	int ans=count_dir(argv[1]);
+
+	cout<<endl;
+	cout<<"All : "<<ans<<endl<<endl;
+
+	return 0;
+}
